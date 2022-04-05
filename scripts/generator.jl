90e70fc0107a92614c557bef635446416084515c
Lukas Schork <60840224+lschork2@users.noreply.github.com>
2022-04-05 11:46:59 +0200

    Add script to generate Julia wrapper around C API.

---
+++ scripts/generator.jl
@@ -0,0 +1,19 @@
+# This script can be used to generate the low-level Julia wrapper for COPT. It
+# requires you to manually set the COPT version number and the path to the
+# appropriate header file.
+
+using Clang.Generators
+
+const COPT_VERSION = "4.0.5"
+const LIBCOPT_HEADERS = ["/usr/local/copt40/include/copt.h"]
+
+const GEN_DIR = joinpath(dirname(@__DIR__), "src", "gen$(COPT_VERSION)")
+if !isdir(GEN_DIR)
+    mkdir(GEN_DIR)
+end
+
+args = get_default_args()
+options = load_options(joinpath(@__DIR__, "generator.toml"))
+options["general"]["output_file_path"] = joinpath(GEN_DIR, "libcopt.jl")
+ctx = create_context(LIBCOPT_HEADERS, args, options)
+build!(ctx)
