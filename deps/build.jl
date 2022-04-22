040f48ff4f0e71d2743e65fb4dd294b290f99922
Lukas Schork <60840224+lschork2@users.noreply.github.com>
2022-04-22 11:08:45 +0200

    Detect location of COPT library when COPT.jl is installed.
    Adapt deps/build.jl from https://github.com/jump-dev/CPLEX.jl (commit
    548c342cd77c518ed0f9d1d5a18008bc9e0dd712) to the standard COPT
    installation.

--- deps/build.jl
+++ deps/build.jl
@@ -1,13 +1,14 @@
-# TODO(lschork2):
-#
-# Automatically detect the location of the COPT library. See
-# https://github.com/jump-dev/CPLEX.jl/blob/master/deps/build.jl
-# https://github.com/jump-dev/Gurobi.jl/blob/master/deps/build.jl
+using Libdl
 
 const _DEPS_FILE = joinpath(dirname(@__FILE__), "deps.jl")
 if isfile(_DEPS_FILE)
     rm(_DEPS_FILE)
 end
+
+const _COPT_VERS = [ # From oldest to most recent.
+    "40",
+]
+const _COPT_HOME_ENV = "COPT_HOME"
 
 function write_depsfile(path)
     open(_DEPS_FILE, "w") do f
@@ -15,4 +16,125 @@ function write_depsfile(path)
     end
 end
 
-write_depsfile("/usr/local/copt/lib/libcopt.so")
+function library_name()
+    prefix = Sys.iswindows() ? "" : "lib"
+    return "$(prefix)copt.$(Libdl.dlext)"
+end
+
+function default_installation_path(copt_home_dir::AbstractString)
+    if Sys.iswindows()
+        return escape_string("C:\\Program Files\\$copt_home_dir")
+    elseif Sys.isapple()
+        return "/Applications/$copt_home_dir"
+    else
+        return "/opt/$copt_home_dir"
+    end
+end
+
+function get_error_message_if_not_found()
+    return """
+    Unable to install COPT.jl.
+
+    The versions of COPT supported by COPT.jl are:
+
+    * 4.0
+
+    You must download and install one of these versions separately.
+
+    You should set the `COPT_HOME` environment variable to point to the install
+    location then try again. For example (updating the path to the correct
+    location):
+
+    ```
+    ENV["COPT_HOME"] = "$(default_installation_path("copt40"))"
+    import Pkg
+    Pkg.add("COPT")
+    Pkg.build("COPT")
+    ```
+
+    See the COPT.jl README at https://github.com/COPT-Public/COPT.jl for further
+    instructions.
+    """
+end
+
+function check_copt_in_libnames(libnames)
+    for l in libnames
+        d = Libdl.dlopen_e(l)
+        if d == C_NULL
+            continue
+        end
+        return l
+    end
+    return nothing
+end
+
+function check_copt_in_environment_variables()
+    # Find COPT in the COPT environment variables.
+    libnames = String[]
+    if haskey(ENV, _COPT_HOME_ENV)
+        for path in split(ENV[_COPT_HOME_ENV], ';')
+            if isdir(path)
+                guessed_file = joinpath(path, "lib", library_name())
+                if isfile(guessed_file)
+                    push!(libnames, guessed_file)
+                end
+            end
+        end
+    end
+    return libnames
+end
+
+function check_copt_in_default_paths()
+    # Find COPT in the default installation locations, based on the platform.
+    copt_home_dirs = String[]
+    for v in reverse(_COPT_VERS)
+        push!(copt_home_dirs, "copt$v")
+    end
+    push!(copt_home_dirs, "COPT")
+
+    libnames = String[]
+    for copt_home_dir in copt_home_dirs
+        path = default_installation_path(copt_home_dir)
+        if isdir(path)
+            guessed_file = joinpath(path, "lib", library_name())
+            if isfile(guessed_file)
+                push!(libnames, guessed_file)
+            end
+        end
+    end
+    return libnames
+end
+
+function try_local_installation()
+    # Iterate through a series of places where COPT could be found: either from
+    # an environment variable or in a default install location, in that order.
+    for libnames in [check_copt_in_environment_variables(), check_copt_in_default_paths()]
+        found_copt_lib = check_copt_in_libnames(libnames)
+        if found_copt_lib !== nothing
+            write_depsfile(Libdl.dlpath(found_copt_lib))
+            @info("Using COPT found in location `$(found_copt_lib)`")
+            return
+        end
+    end
+
+    error(get_error_message_if_not_found())
+end
+
+function try_ci_installation()
+    error("CI installation is not supported")
+    # CPLEX_VERSION = ENV["CPLEX_VERSION"]
+    # url = ENV["SECRET_CPLEX_URL_" * CPLEX_VERSION]
+    # local_filename = joinpath(@__DIR__, "libcplex.so")
+    # download(url, local_filename)
+    # write_depsfile(local_filename)
+end
+
+if get(ENV, "JULIA_REGISTRYCI_AUTOMERGE", "false") == "true"
+    # We need to be able to install and load this package without error for
+    # Julia's registry AutoMerge to work. Just write a fake libcopt path.
+    write_depsfile("julia_registryci_automerge")
+elseif get(ENV, "SECRET_CPLEX_URL_12100", "") != ""
+    try_ci_installation()
+else
+    try_local_installation()
+end
