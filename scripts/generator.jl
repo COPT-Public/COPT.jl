b22703409c763cc601d8a703c7e6355e8d9decd1
Lukas Schork <60840224+lschork2@users.noreply.github.com>
2022-04-26 15:07:02 +0200

    Document how to upgrade COPT.jl to a new COPT version.

--- scripts/generator.jl
+++ scripts/generator.jl
@@ -5,7 +5,7 @@
 using Clang.Generators
 
 const COPT_VERSION = "4.0.6"
-const LIBCOPT_HEADERS = ["/opt/copt40/include/copt.h"]
+const COPT_HEADERS = ["/opt/copt40/include/copt.h"]
 
 const GEN_DIR = joinpath(dirname(@__DIR__), "src", "gen$(COPT_VERSION)")
 if !isdir(GEN_DIR)
@@ -15,5 +15,5 @@ end
 args = get_default_args()
 options = load_options(joinpath(@__DIR__, "generator.toml"))
 options["general"]["output_file_path"] = joinpath(GEN_DIR, "libcopt.jl")
-ctx = create_context(LIBCOPT_HEADERS, args, options)
+ctx = create_context(COPT_HEADERS, args, options)
 build!(ctx)
