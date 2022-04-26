e9d43e43fbc586d200895b4b5fde4dc9bad6f40e
odow <o.dowson@gmail.com>
2022-04-26 14:49:49 +1200

    Update LICENSE to acknowledge CPLEX.jl and add headers to files

--- test/MathOptInterface/MOI_wrapper.jl
+++ test/MathOptInterface/MOI_wrapper.jl
@@ -1,3 +1,9 @@
+# Copyright (c) 2013: Joey Huchette and contributors
+# Copyright (c) 2022: COPT-Public
+#
+# Use of this source code is governed by an MIT-style license that can be found
+# in the LICENSE.md file or at https://opensource.org/licenses/MIT.
+
 module TestMOIwrapper
 
 using COPT
