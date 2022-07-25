8f9d550e2e90e1f05bba36e6d0e95997f4b5226f
Lukas Schork <60840224+lschork2@users.noreply.github.com>
2022-07-25 10:24:21 +0200

    Adjust runtests.jl to allow executing it from any directory.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,7 @@
 using Test
 
 @testset "MathOptInterface Tests" begin
-    for file in readdir("MathOptInterface")
+    for file in readdir(joinpath(@__DIR__, "MathOptInterface"))
         include(joinpath("MathOptInterface", file))
     end
 end
