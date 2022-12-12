929e99fbc8e8ed17659036055c1090aab2305e64
Lukas Schork <60840224+lschork2@users.noreply.github.com>
2022-12-12 11:45:48 +0100

    Re-write generator for the low-level Julia wrapper.
    - Put code into a module.
    - Replace constants by function arguments.
    - Automatically download COPT package when no header file is given.

--- scripts/generator.jl
+++ scripts/generator.jl
@@ -1,19 +1,46 @@
-# This script can be used to generate the low-level Julia wrapper for COPT. It
-# requires you to manually set the COPT version number and the path to the
-# appropriate header file.
+module Generator
 
-using Clang.Generators
+import Clang.Generators as ClangGen
+import Downloads
+import Inflate
+import Tar
 
-const COPT_VERSION = "6.0.1"
-const COPT_HEADERS = ["/opt/copt60/include/copt.h"]
+"""
+    generate_wrapper(copt_version::VersionNumber, header_file::AbstractString="")
 
-const GEN_DIR = joinpath(dirname(@__DIR__), "src", "gen$(COPT_VERSION)")
-if !isdir(GEN_DIR)
-    mkdir(GEN_DIR)
+Generates the low-level Julia wrapper for the given COPT version in directory
+src/genX.Y.Z. `header_file` is the path to the C header file `copt.h` from which
+the wrapper is built. If empty, then the COPT package is downloaded and
+extracted to temporary locations.
+"""
+function generate_wrapper(
+    copt_version::VersionNumber,
+    header_file::AbstractString = "",
+)
+    # Create output directory.
+    gen_dir = joinpath(dirname(@__DIR__), "src", "gen$(copt_version)")
+    if !isdir(gen_dir)
+        mkdir(gen_dir)
+    end
+    # If no header file is given, then download and extract the COPT package.
+    if isempty(header_file)
+        url = "https://pub.shanshu.ai/download/copt/$(copt_version)/linux64/CardinalOptimizer-$(copt_version)-lnx64.tar.gz"
+        println("Downloading ", url)
+        download_path = Downloads.download(url)
+        extract_path =
+            Tar.extract(IOBuffer(Inflate.inflate_gzip(download_path)))
+        contents = readdir(extract_path)
+        @assert length(contents) == 1
+        copt_dir = contents[1]
+        header_file = joinpath(extract_path, copt_dir, "include", "copt.h")
+    end
+    # Run Clang generator.
+    args = ClangGen.get_default_args()
+    options = ClangGen.load_options(joinpath(@__DIR__, "generator.toml"))
+    options["general"]["output_file_path"] = joinpath(gen_dir, "libcopt.jl")
+    ctx = ClangGen.create_context(header_file, args, options)
+    ClangGen.build!(ctx)
+    return nothing
 end
 
-args = get_default_args()
-options = load_options(joinpath(@__DIR__, "generator.toml"))
-options["general"]["output_file_path"] = joinpath(GEN_DIR, "libcopt.jl")
-ctx = create_context(COPT_HEADERS, args, options)
-build!(ctx)
+end # module
